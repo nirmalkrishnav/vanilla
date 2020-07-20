ee8058aa4d56916c8a02867f5a1f0a08b17aee4a
meuequalsD <nirmalkrishnav@gmail.com>
2020-07-20 23:09:47 +0530

    added some layout chagnes

--- src/app/layouts/Navbar.js
+++ src/app/layouts/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
     render() {
         return (
-            <div>
+            <div className="top-tabs">
                 <div>
                     <Link to="/home">Home</Link>
                 </div>

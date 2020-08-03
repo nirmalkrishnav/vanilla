0925811a2761e3836557be60bb8d0bd81ad3b593
meuequalsD <nirmalkrishnav@gmail.com>
2020-08-03 22:21:16 +0530

    icon count in top

--- src/app/layouts/Navbar.js
+++ src/app/layouts/Navbar.js
@@ -1,44 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AiFillCalculator, AiFillCrown, AiFillFire, AiOutlineMore } from 'react-icons/ai';
-import { BsFillDiamondFill } from 'react-icons/bs';
+import { AiFillGithub, AiFillCalculator, AiFillCrown, AiFillFire, AiOutlineMore } from 'react-icons/ai';
 import { IconContext } from "react-icons";
+import './layout.scss';
 
 
 class Navbar extends React.Component {
     render() {
         return (
-            <div className="flex flex-row content-start justify-around items-center px-3">
+            <div className="flex flex-row content-start justify-between items-center p-3 top-tabs">
                 <div>
-                    <IconContext.Provider value={{ color: "black", size: "32px" }}>
-                        <div>
-                            <AiFillCalculator />
-                        </div>
+                    <IconContext.Provider value={{ color: "#afafaf", size: "32px" }}>
+                        <AiFillCalculator />
                     </IconContext.Provider>
                 </div>
-                <div>
-                    <IconContext.Provider value={{ color: "black", size: "32px" }}>
-                        <div>
-                            <AiFillCrown />
-                        </div>
+                <div className="flex">
+                    <IconContext.Provider value={{ color: "#afafaf", size: "32px" }}>
+                        <AiFillCrown />
                     </IconContext.Provider>
+                    0
+                </div>
+                <div className="flex">
+                    <IconContext.Provider value={{ color: "#afafaf", size: "32px" }}>
+                        <AiFillFire />
+                    </IconContext.Provider>
+                    0
+                </div>
+                <div className="flex">
+                    <IconContext.Provider value={{ color: "#afafaf", size: "32px" }}>
+                        <AiFillGithub />
+                    </IconContext.Provider>
+                    0
                 </div>
                 <div>
-                    <IconContext.Provider value={{ color: "black", size: "32px" }}>
-                        <div>
-                            <AiFillFire />
-                        </div>
-                    </IconContext.Provider>
-                </div>
-                <div>
-                    <IconContext.Provider value={{ color: "black", size: "32px" }}>
-                        <div>
-                            <BsFillDiamondFill />
-                        </div>
-                    </IconContext.Provider>
-                </div>
-                <div>
-                    <IconContext.Provider value={{ color: "black", size: "32px" }}>
+                    <IconContext.Provider value={{ color: "#afafaf", size: "32px" }}>
                         <div>
                             <AiOutlineMore />
                         </div>
